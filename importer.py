8c54f16200b726861fd745bb40937fe71ea237cd
Romain Guimbal <39882829+RomainGuimbal@users.noreply.github.com>
2024-07-12 01:04:49 +0200

    import nurbs first step

--- importer.py
+++ importer.py
@@ -93,7 +93,7 @@ def build_SP_curve(brepEdge, collection, context) :
 
 
 
-def build_SP_patch(brepFace, collection, context) :
+def build_SP_bezier_patch(brepFace, collection, context) :
     status =""
     face = BRep_Tool.Surface(brepFace)
 
@@ -108,26 +108,9 @@ def build_SP_patch(brepFace, collection, context) :
                 pole = bezier_surface.Pole(u, v)
                 vector_pts[u-1, v-1] = Vector((pole.X()/1000, pole.Y()/1000, pole.Z()/1000))
 
-    elif face.DynamicType().Name() == "Geom_BSplineSurface":
-        bspline_surface = Geom_BSplineSurface.DownCast(face)
-        u_count, v_count = bspline_surface.NbUPoles(), bspline_surface.NbVPoles()
-
-        u_knots = [bspline_surface.UKnot(i+1) for i in range(bspline_surface.NbUKnots())]
-        v_knots = [bspline_surface.VKnot(i+1) for i in range(bspline_surface.NbVKnots())]
-        if any(x not in [0.0, 1.0] for x in u_knots) or any(x not in [0.0, 1.0] for x in v_knots):
-            status = "Unsupported NURBS feature present, imported step will not be exact"
-            print(u_knots)
-            print(v_knots)
-
-        vector_pts = np.zeros((u_count, v_count), dtype=Vector)
-        for u in range(1, u_count + 1):
-            for v in range(1, v_count + 1):
-                pole = bspline_surface.Pole(u, v)
-                vector_pts[u-1, v-1] = Vector((pole.X()/1000, pole.Y()/1000, pole.Z()/1000))
-
-                weight = bspline_surface.Weight(u, v)
+                weight = bezier_surface.Weight(u, v)
                 if weight!=1.0:
-                    status = "Weighted NURBS present, SP doesn't currently support them"
+                    status = "Weighted Bezier not unsupported"
     else:
         print("error on face type")
         return False
@@ -154,6 +137,74 @@ def build_SP_patch(brepFace, collection, context) :
     if status != "":
         print(status)
     return True
+
+
+
+
+
+def build_SP_NURBS_patch(brepFace, collection, context):
+    status =""
+    face = BRep_Tool.Surface(brepFace)
+
+    if face.DynamicType().Name() == "Geom_BSplineSurface":
+        bspline_surface = Geom_BSplineSurface.DownCast(face)
+        u_count, v_count = bspline_surface.NbUPoles(), bspline_surface.NbVPoles()
+
+        custom_knot = False
+        u_knots = [bspline_surface.UKnot(i+1) for i in range(bspline_surface.NbUKnots())]
+        v_knots = [bspline_surface.VKnot(i+1) for i in range(bspline_surface.NbVKnots())]
+        if any(x not in [0.0, 1.0] for x in u_knots) or any(x not in [0.0, 1.0] for x in v_knots):
+            custom_knot = True
+
+        custom_weight = False
+        vector_pts = np.zeros((u_count, v_count), dtype=Vector)
+        for u in range(1, u_count + 1):
+            for v in range(1, v_count + 1):
+                pole = bspline_surface.Pole(u, v)
+                vector_pts[u-1, v-1] = Vector((pole.X()/1000, pole.Y()/1000, pole.Z()/1000))
+
+                weight = bspline_surface.Weight(u, v)
+                if weight!=1.0:
+                    custom_weight = True
+    else:
+        print("error on face type")
+        return False
+
+    # create object
+    mesh = bpy.data.meshes.new("Patch CP")
+    vert, edges, faces = create_grid(vector_pts)
+    mesh.from_pydata(vert, edges, faces)
+    ob = bpy.data.objects.new('STEP Patch', mesh)
+    
+    # set smooth
+    mesh = ob.data
+    values = [True] * len(mesh.polygons)
+    mesh.polygons.foreach_set("use_smooth", values)
+
+    # add_modifier(ob, "SP - NURBS Patch Meshing")
+    collection.objects.link(ob)
+    bpy.context.view_layer.objects.active = ob
+
+    # TODO
+    # if custom_knot:
+        # Add vertex groups
+        # add modifier
+        # assign vertex groups to modifier
+
+    # if custom_weight:
+        # Add vertex group
+        # add modifier
+        # assign vertex group to modifier
+    
+    bpy.ops.object.modifier_add_node_group(asset_library_type='CUSTOM',
+                                        asset_library_identifier="SurfacePsycho",
+                                        relative_asset_identifier="assets.blend\\NodeTree\\SP - NURBS Patch Meshing")
+    
+    if status != "":
+        print(status)
+    return True
+
+
 
 
 
@@ -229,8 +280,10 @@ def recursive_SP_from_brep_shape(shape, collection, context, enabled_entities, v
         print(f"{tab}Face")
         if enabled_entities["faces"]:
             ft= face_type(shape)
-            if ft=="Bezier" or ft=="NURBS":
-                build_SP_patch(shape, collection, context)
+            if ft=="Bezier":
+                build_SP_bezier_patch(shape, collection, context)
+            elif ft=="NURBS":
+                build_SP_NURBS_patch(shape, collection, context)
             elif ft=="Plane":
                 build_SP_flat(shape, collection, context)
             else :
