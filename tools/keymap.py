ad922034a7e0dcc3e803ad765a44236692480c8e
Romain Guimbal <39882829+RomainGuimbal@users.noreply.github.com>
2026-07-22 22:02:37 +0200

    left click grrr

--- tools/keymap.py
+++ tools/keymap.py
@@ -1,18 +1,30 @@
 OBJ_KEYMAP = (
-    ("view3d.segment_select_click", {"type": "LEFTMOUSE", "value": "PRESS"}, None),
     (
         "view3d.segment_select_click",
-        {"type": "LEFTMOUSE", "value": "PRESS", "shift": True},
+        {"type": "LEFTMOUSE", "value": "PRESS", "ctrl": True},
+        None,
+    ),
+    (
+        "view3d.segment_select_click",
+        {"type": "LEFTMOUSE", "value": "PRESS", "ctrl": True, "shift": True},
         None,
     ),
     ("object.sp_blend_surfaces", {"type": "B", "value": "PRESS", "ctrl": True}, None),
     # ("object.sp_toggle_control_geom", {"type": "V", "value": "PRESS"}, None),
     ("object.sp_extract_segment", {"type": "E", "value": "PRESS"}, None),
     # ("wm.call_menu_pie", {"type": "F", "value": "PRESS", "shift": True}, None),
-    ("object.sp_toggle_control_geom", {"type": "F", "value": "PRESS", "shift": True}, None),
+    (
+        "object.sp_toggle_control_geom",
+        {"type": "F", "value": "PRESS", "shift": True},
+        None,
+    ),
     ("object.sp_add_isoparam", {"type": "I", "value": "PRESS"}, None),
 )
 
 EDIT_KEYMAP = (
-    ("wm.call_menu_pie", {"type": "F", "value": "PRESS", "shift": True}, {"properties": [("name", "SP_MT_PIE_SegmentEdit")]}),
+    (
+        "wm.call_menu_pie",
+        {"type": "F", "value": "PRESS", "shift": True},
+        {"properties": [("name", "SP_MT_PIE_SegmentEdit")]},
+    ),
 )
